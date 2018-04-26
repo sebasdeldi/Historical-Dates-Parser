795c1e960df7833773d03090c544b83b6abb136c
Sebastian <sebasdeldi123@gmail.com>
2018-04-26 17:35:52 -0400

    fixed month dats

--- parser.rb
+++ parser.rb
@@ -9,21 +9,20 @@ data = File.new("historicalData.txt", "w+")
 months.each do |month|
 	if month == "February"
 		days = 28
-	elsif month == "April" || "June" || "September" || "November"
-		days = 31
-	else
+	elsif month == "April" || month == "June" || month == "September" || month == "November"
 		days = 30
+	else
+		days = 31
 	end
+
 	(1..days).each do |day|
-		data.puts "** SEARCHING DATA FOR " + month.to_s + ' / ' + day.to_s + " **"
-		puts "** SEARCHING DATA FOR " + month.to_s + ' / ' + day.to_s + " **"
+		data.puts "** DATA FROM " + month.to_s + ' / ' + day.to_s + " **"
+		puts "** SCRAPING DATA FROM " + month.to_s + ' / ' + day.to_s + " **"
 		link = 'https://en.wikipedia.org/wiki/'+month.to_s+'_' + day.to_s
 		doc = Nokogiri::HTML(open(link, {ssl_verify_mode: 0}))
 		doc.css('h2:nth-of-type(1) + ul li').each do |link|
 			data.puts link.content
-		  	puts link.content
 		end
 		data.puts "--------------------------------------------------------------------------------------------"
-		puts "--------------------------------------------------------------------------------------------"
 	end
 end
